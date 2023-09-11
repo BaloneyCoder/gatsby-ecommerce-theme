b37475e412712c0ea0da27b5f7a4e1d2036c3dbe
BaloneyCoder <137069942+BaloneyCoder@users.noreply.github.com>
2023-09-11 19:35:34 +0700

    Update index.js
    Test 2 Sub

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Test'}
+        title={'Test2'}
         subtitle={'Discover Autumn Winter 2021'}
         ctaText={'shop now'}
         ctaAction={goToShop}
